6647db64b24d5a0805ab6d0764e044f19702e1d2
Liudz <lwz1815317@gmail.com>
2022-07-24 16:11:48 +0800

    1.0

---
+++ blog-app/src/components/card/CardMe.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-card>
+    <h1 class="me-author-name">Blog | 学习 生活
+    </h1>
+    <div class="me-author-description">
+      <span><i class="el-icon-location-outline">   </i>&nbsp; 中国·广东·广州</span>
+      <span><i class="me-icon-job"></i> &nbsp;同性交友</span>
+    </div>
+    <div class="me-author-tool">
+      <i @click="showTool(qq)" :title="qq.title" class="iconfont icon-qq"></i>
+      <i @click="showTool(github)" :title="github.title" class="iconfont icon-bilibili-fill"></i>
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardMe',
+    data() {
+      return {
+        qq: {title: 'QQ', message: '1018484094'},
+        github: {
+          title: 'Github',
+          message: '<a target="_blank" href="https://github.com/xiaoyaoguai6">https://github.com/xiaoyaoguai6</a>'
+        }
+      }
+    },
+    methods: {
+      showTool(tool) {
+        this.$message({
+          duration: 0,
+          showClose: true,
+          dangerouslyUseHTMLString: true,
+          message: '<strong>' + tool.message + '</strong>'
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .me-author-name {
+    text-align: center;
+    font-size: 30px;
+    border-bottom: 1px solid #5FB878;
+  }
+
+  .me-author-description {
+    padding: 8px 0;
+  }
+
+  .me-icon-job {
+    padding-left: 30px;
+  }
+
+  .me-author-tool {
+    text-align: center;
+    padding-top: 10px;
+  }
+
+  .me-author-tool i {
+    cursor: pointer;
+    padding: 4px 10px;
+    font-size: 30px;
+  }
+</style>
